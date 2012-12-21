c6d74286b28bc64bb35fe42e7c435226625398f8
ToreB <toreb27@gmail.com>
2012-12-21 10:25:20 +0100

    la til filer som egentlig skulle vært lagt til

---
+++ src/main/java/no/mesan/document_merging/App.java
@@ -0,0 +1,94 @@
+package no.mesan.document_merging;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.docx4j.openpackaging.exceptions.Docx4JException;
+
+public class App {
+    public static void main( String[] args ) {
+		String path = "/media/sf_DATA_DRIVE/Documents/Jobb/Forskningsraadet/";
+		String layout = path + "template.docx";
+		String contents = path + "IMMedInnhold.docx";
+    	
+        try {
+        	//DocxDocument docx = replaceStyledPlaceholders(path + "template2.docx");
+        	
+			//DocxDocument docx = mergeDocuments(layout, contents);
+       	
+        	DocxDocument docx = insertFromPlaceholderBlocks(path + "FlettemotorData.docx");
+        	
+			docx.writeToFile(path + "dokument.docx");
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (Docx4JException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    private static DocxDocument replaceStyledPlaceholders(String layout) 
+    		throws FileNotFoundException, Docx4JException {
+    	DocxDocument layoutTemplate = new DocxDocument(layout);
+		
+		Map<String, String> headerReplacements = new HashMap<String, String>();
+		headerReplacements.put("<%HEADER_MIDDLE%>", "Dette er en header!");
+		headerReplacements.put("<%HEADER_LEFT%>", "Venstre side i header");
+		headerReplacements.put("<%HEADER_RIGHT%>", "Høyre side i header!");		
+		layoutTemplate.replaceHeaderPlaceholders(headerReplacements);
+		
+		layoutTemplate.replaceFooterPlaceholder("<%FOOTER_LEFT%>", "Venstre side i footer.");
+		layoutTemplate.replaceFooterPlaceholder("<%FOOTER_MIDDLE%>", "Dette er en footer");
+		
+		Map<String, String> bodyReplacements = new HashMap<String, String>();
+		bodyReplacements.put("<%TITLE%>", "Min Tittel");
+		bodyReplacements.put("<%CONTENT_NORMAL%>", "Normal skrift");
+		bodyReplacements.put("<%CONTENT_BOLD%>", "Fet skrift");
+		bodyReplacements.put("<%CONTENT_ITALIC%>", "Kursiv skrift");
+		bodyReplacements.put("<%CONTENT_UNDERLINED%>", "Understreket skrift");
+		bodyReplacements.put("<%CONTENT_BOLD_ITALIC_UNDERLINED%>", "Fet, kursiv, understreket skrift");
+		
+		String placeholder = "<Denne lange setningen kan også brukes som en ‘placeholder’, hvis man måtte ønske det, men det er kanskje ikke like lett å få øye på den.>";
+		bodyReplacements.put(placeholder, "Denne setningen ble satt inn fra fletteprogrammet :).");
+		
+		layoutTemplate.replaceBodyPlaceholders(bodyReplacements);
+		
+		return layoutTemplate;
+    }
+
+	private static DocxDocument insertFromPlaceholderBlocks(String documentWithBLocks)
+			throws FileNotFoundException, Docx4JException {
+		DocxDocument docx = new DocxDocument();
+		
+		DocxDocument data = new DocxDocument(documentWithBLocks);
+		docx.insertElementsFromContentBlock(data.getDocument(), "<title>", "</title>");
+		docx.insertElementsFromContentBlock(data.getDocument(), "<content>", "</content>");
+		docx.insertElementsFromContentBlock(data.getDocument(), "<images>", "</images>");
+		
+		return docx;
+	}
+    
+    private static DocxDocument mergeDocuments(String layoutTemplatePath, String contentTemplateWithContentPath) 
+    		throws IOException, Docx4JException {
+    	DocxDocument layoutTemplate = new DocxDocument(layoutTemplatePath);
+		DocxDocument contentsTemplate = new DocxDocument(contentTemplateWithContentPath);
+		
+		layoutTemplate.replacePlaceholderWithDocumentContent("<%CONTENT%>", contentsTemplate.getDocument());		
+		
+		Map<String, String> headerReplacements = new HashMap<String, String>();
+		headerReplacements.put("<%HEADER_MIDDLE%>", "Dette er en header!");
+		headerReplacements.put("<%HEADER_LEFT%>", "Venstre side i header");
+		headerReplacements.put("<%HEADER_RIGHT%>", "Høyre side i header!");
+		
+		layoutTemplate.replaceHeaderPlaceholders(headerReplacements);
+		layoutTemplate.replaceFooterPlaceholder("<%FOOTER_LEFT%>", "Venstre side i footer.");
+		layoutTemplate.replaceFooterPlaceholder("<%FOOTER_MIDDLE%>", "Dette er en footer");
+		layoutTemplate.replaceBodyPlaceholder("<%TITLE%>", "Min Tittel");
+		
+		return layoutTemplate;
+    }
+}
---
+++ src/test/java/no/mesan/document_merging/DocxDocumentTest.java
@@ -0,0 +1,73 @@
+package no.mesan.document_merging;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import no.mesan.document_merging.DocxDocument;
+
+import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Categories.ExcludeCategory;
+
+public class DocxDocumentTest {
+	private DocxDocument doc;
+	
+	@Test
+	public void testDefaultCtor_createsdocument() {		
+		doc = new DocxDocument();
+		
+		assertNotNull(doc.getDocument());
+	}
+
+	@Test
+	public void testParameterizedCtor_WhenGivenAValidFilePath_ShouldNotThrowException() {
+		try {
+			doc = new DocxDocument("/media/sf_DATA_DRIVE/Documents/jobb/Forskningsraadet/template.docx");			
+		} catch(Exception e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void testParameterizedCtor_WhenGivenAnInvalidFilePath_ShouldThrowException() {
+		try {
+			doc = new DocxDocument("invalid/file/path.docx");
+			
+			fail();
+		} catch(Exception e) {	}
+	}
+
+	@Test
+	public void testWriteToFile_ShouldNotThrowException() {
+		writeToFile("testDocument.docx");
+	}
+	
+	@Test
+	public void testWriteToPDF_ShouldNotThrowException() {
+		writeToFile("testDocument.pdf");
+	}
+	
+	private void writeToFile(String filename) {	
+		doc = new DocxDocument();
+		try {
+			doc.writeToFile(filename);
+		} catch (Exception e) {
+			fail();
+		}
+		
+		File file = new File(filename);
+		if (!file.exists()) {
+			fail("File should exist");
+		} else { //File exists, so clean up
+			try {
+				file.delete();
+			} catch(SecurityException e) {
+				//Don't really care, file will get overwritten anyway
+			}
+		}
+	}
+}
